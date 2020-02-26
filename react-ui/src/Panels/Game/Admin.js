828fa15978e8e221638a02b4ebaf636736826f5b
Seba <sebacej.92@gmail.com>
2020-02-26 15:13:00 +0100

    Fixed readonly attribute typo

--- react-ui/src/Panels/Game/Admin.js
+++ react-ui/src/Panels/Game/Admin.js
@@ -177,7 +177,7 @@ class Admin extends React.Component {
                                     id="standard-basic" 
                                     label="Response to question"
                                     multiline
-                                    readonly
+                                    readOnly
                                     value={this.state.responseToQuestion.text}
                                     rows="4"
                                 />
